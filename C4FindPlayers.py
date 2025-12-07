abd9d5c9368098c063ce58e6d51d8ec9a9d12347
Mimalynx <johan.n.daun@gmail.com>
2025-12-07 02:14:56 +0100

    Write to textfile all players

--- C4FindPlayers.py
+++ C4FindPlayers.py
@@ -33,58 +33,75 @@ wait = WebDriverWait(driver, 20)
 
 listOfAllIds = ["6034f6c4e8d38d66cf92b549"]
 listOfIds = ["6034f6c4e8d38d66cf92b549"]
+NameList = ["angel 77"]
 start = True
 
 while(len(listOfIds) > 0):
+    try:
+        print(len(listOfIds))
+        print(len(listOfAllIds))
 
-    print(len(listOfIds))
-    print(len(listOfAllIds))
-
-    idOfProfile = listOfIds.pop()
-    driver.get("https://papergames.io/en/match-history/" + idOfProfile)
-    time.sleep(0.5)
-    if start == True:
-        consent =  wait.until(EC.element_to_be_clickable(
-            (By.XPATH, "//button[contains(@class, 'fc-cta-consent')]")
-        ))
-        consent.click()
-        start = False
-
-    minElo = 1850
-    select50()
-    time.sleep(0.5)
-    rows = driver.find_elements(By.CSS_SELECTOR, "div.user-profile.cursor-pointer")
-    eloList = driver.find_elements(By.CLASS_NAME, "c-gray-700")[1:]
-    elos = [int(el.text[1:-1]) for el in eloList]
-
-    for i in range(len(rows)):
-        if elos[i] < minElo:
-            continue
-        row = rows[i]
-        #row.click()
-        driver.execute_script("arguments[0].click();", row)
-        time.sleep(0.2)
-        button = wait.until(
-            EC.element_to_be_clickable((
-                By.CSS_SELECTOR,
-                "button.mat-mdc-menu-trigger"
+        idOfProfile = listOfIds.pop()
+        driver.get("https://papergames.io/en/match-history/" + idOfProfile)
+        time.sleep(0.5)
+        if start == True:
+            consent =  wait.until(EC.element_to_be_clickable(
+                (By.XPATH, "//button[contains(@class, 'fc-cta-consent')]")
             ))
-        )
-        driver.execute_script("arguments[0].click();", button)
-        history_item = wait.until(
-            EC.element_to_be_clickable((
-                By.XPATH,
-                "//a[@mat-menu-item]//span[text()='History']/parent::a"
-            ))
-        )
-        href = history_item.get_attribute("href")
-        candidateUser = href[39:]
-        if(not(candidateUser in listOfAllIds)):
-            listOfAllIds.append(candidateUser)
-            listOfIds.append(candidateUser)
-            print(candidateUser)
-            print(elos[i])
-        time.sleep(0.2)
+            consent.click()
+            start = False
+
+        minElo = 1850
+        select50()
+        time.sleep(0.5)
+        rows = driver.find_elements(By.CSS_SELECTOR, "div.user-profile.cursor-pointer")
+        eloList = driver.find_elements(By.CLASS_NAME, "c-gray-700")[1:]
+        elos = [int(el.text[1:-1]) for el in eloList]
+
+        name_elements = driver.find_elements(By.CLASS_NAME, "user-profile")
+        names = [el.text for el in name_elements]
+
+        for i in range(len(rows)):
+            try:
+                if elos[i] < minElo:
+                    continue
+                if names[i] in NameList:
+                    continue
+                row = rows[i]
+                #row.click()
+                driver.execute_script("arguments[0].click();", row)
+                time.sleep(0.2)
+                button = wait.until(
+                    EC.element_to_be_clickable((
+                        By.CSS_SELECTOR,
+                        "button.mat-mdc-menu-trigger"
+                    ))
+                )
+                driver.execute_script("arguments[0].click();", button)
+                history_item = wait.until(
+                    EC.element_to_be_clickable((
+                        By.XPATH,
+                        "//a[@mat-menu-item]//span[text()='History']/parent::a"
+                    ))
+                )
+                href = history_item.get_attribute("href")
+                candidateUser = href[39:]
+                if(not(candidateUser in listOfAllIds)):
+                    listOfAllIds.append(candidateUser)
+                    listOfIds.append(candidateUser)
+                    NameList.append(names[i])
+                    print(candidateUser)
+                    print(elos[i])
+                    try:
+                        with open("ListOfPlayers.txt", "a") as myfile:
+                            myfile.write(candidateUser + "\n")
+                    except Exception as e:
+                        print("File write error:", e)
+                time.sleep(0.2)
+            except:
+                continue
+    except:
+        continue
     
 
 
